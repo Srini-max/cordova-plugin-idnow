df6c6c78c3dfa51a532a999f7ab3004e2b743845
Srinivas <54628670+Srini-max@users.noreply.github.com>
2020-04-30 03:00:48 +0400

    SDK updated

--- src/ios/IDnowPlugin.m
+++ src/ios/IDnowPlugin.m
@@ -1,7 +1,7 @@
 /********* IDnowPlugin Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
-@import IDnowSDK;
+@import IDNowSDKCore;
 
 /********* IDnowPlugin.h Intefrace *******/
 
@@ -34,82 +34,25 @@ NSTimer *keepAliveTimer;
 - (void)startPhotoIdent:(CDVInvokedUrlCommand*)command {
     
     globalCommand = command;
-    
-    
-    // Set up and customize settings
-    self.settings = [IDnowSettings new];
-    self.settings.showErrorSuccessScreen = true;
-    self.settings.showVideoOverviewCheck = true;
-    
-    
-    COMPANY_ID_PHOTO_IDENT = [globalCommand.arguments objectAtIndex:0];
-    TRANSACTION_TOKEN_PHOTO_IDENT = [globalCommand.arguments objectAtIndex:1];
-    SHOW_ERROR_SUCCESS_SCREEN = [globalCommand.arguments objectAtIndex:2];
-    
-    // Setting dummy dev token and company id -> will instantiate a photo identification
-    self.settings.transactionToken = TRANSACTION_TOKEN_PHOTO_IDENT;
-    self.settings.companyID = COMPANY_ID_PHOTO_IDENT;
-    self.settings.showErrorSuccessScreen = SHOW_ERROR_SUCCESS_SCREEN;
-    
-    // This time we use the delegate instead of blocks (it's your choice)
-    self.idnowController.delegate = self;
-    
-    // Initialize identification
-    [self.idnowController initialize];
+    [IDNowSDK.shared startWithToken:@"TND-PLMSE" preferredLanguage:@"en" fromViewController:self listener:idnowResultListener];
+
 }
+void (^idnowResultListener)(enum IdentResult identResult, NSString * _Nonnull) = ^(enum IdentResult result, NSString* message) {
+      NSLog( @"SDK finished");
 
-#pragma mark - IDnowControllerDelegate -
+      if (result == IdentResultERROR) {
+          // show result in debug log
+          CDVPluginResult* pluginResult = nil;
+          pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Some error occured"];
+          [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+      }
 
-- (void) idnowController: (IDnowController *) idnowController initializationDidFailWithError: (NSError *) error
-{
-    // Initialization failed -> Present an alert containing localized error description
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle: @"Error" message: error.localizedDescription preferredStyle: UIAlertControllerStyleAlert];
-    UIAlertAction	  *action		   = [UIAlertAction actionWithTitle: @"Ok" style: UIAlertActionStyleCancel handler: nil];
-    [alertController addAction: action];
-    [self.viewController presentViewController: alertController animated: true completion: nil];
-}
-
-
-- (void) idnowControllerDidFinishInitializing: (IDnowController *) idnowController
-{
-    // Initialization was successfull -> Start identification
-    [self.idnowController startIdentificationFromViewController: self];
-}
-
-
-- (void) idnowControllerCanceledByUser: (IDnowController *) idnowController
-{
-    // The identification was canceled by the user.
-    // For example the user tapped on the "x"-Button or simply navigates back.
-    // Normally you don't have to do anything...
-    [self invalidateTimer];
-    CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Identification Canceled By User"];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:globalCommand.callbackId];
-}
-
-
-- (void) idnowController: (IDnowController *) idnowController identificationDidFailWithError: (NSError *) error
-{
-    // Identification failed
-    // If showErrorSuccessScreen (Settings) is disabled and error.type == IDnowErrorTypeIdentificationFailed
-    // you can show for example an alert to your users.
-    [self invalidateTimer];
-    CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.description];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:globalCommand.callbackId];
-}
-
-
-- (void) idnowControllerDidFinishIdentification: (IDnowController *) idnowController
-{
-    // Identification was successfull
-    // If showErrorSuccessScreen (Settings) is disabled
-    // you can show for example an alert to your users.
-    [self invalidateTimer];
-    CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Identification Finished"];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:globalCommand.callbackId];
-}
+      if( result == IdentResultFINISHED ) {
+          // show result in debug log
+          CDVPluginResult* pluginResult = nil;
+          pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Identification Finished"];
+          [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+      }
+  };
 
 @end
